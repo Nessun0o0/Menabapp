0dcc3156db0490138b82ad41d54c2932e266c28b
Nessun0o0 <101934093+Nessun0o0@users.noreply.github.com>
2023-09-06 14:07:56 +0200

    Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import logo from './logo.svg';
 import './App.css';
+import Canvas from './Canvas';
 
 function App() {
+
+  const [text,setText] = useState("")
+
+  const handleTextChange = event => {
+    setText(event.target.value)
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <textarea id='text' name='text' value={text} onChange={handleTextChange}>  </textarea>
+      <TransformWrapper>
+        <TransformComponent>
+          <img src={logo} alt='test' className='prova'></img>
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 }
---
+++ src/Page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Page.css"
+
+function Page() {
+    return (
+        <div className="A4">
+            <p>bella zio</p>
+        </div>
+    )
+}
+
+export default Page
