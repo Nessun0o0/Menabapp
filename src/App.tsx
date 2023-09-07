355c2611edbda5c5d389d522e84e4ceb0c880524
Nessun0o0 <francescopalomb@gmail.com>
2023-09-07 11:31:56 +0200

    Changed controls and added page counter

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import PDF from './pdf';
 
 function App() {
 
-  const [text,setText] = useState("#Copertina\n#Retro")
+  const [text,setText] = useState("Copertina\nRetro")
   const [pages, setPages] = useState([<div />, <Page className={"c-left"} title={"Copertina"} content={""}/>, <Page className={"c-right"} title={"Retro"} content={""}/>, <div />])
+  const [counter, setCounter] = useState(1)
   const classNames = ["left", "c-left", "c-right", "right"]
   const pdfRef = useRef()
 
@@ -20,16 +21,18 @@ function App() {
     let lines = text.split("\n")
     let array = [<div />]
     for (let i = 0; i < lines.length; i++) {
-      if (lines[i][0] == "#") {
-        let content = ""
-        if (lines[i+1] !== undefined && lines[i+1][0] !== "#") {
-          content = lines[i+1]
-        }
-        array.push(<Page className={classNames[array.length % 4]} title={lines[i].slice(1)} content={content}/>)
+      if (lines[i][0] == "-") {
+        continue
       }
+      let content = ""
+      if (lines[i+1] !== undefined && lines[i+1][0] === "-") {
+        content = lines[i+1].slice(1)
+      }
+      array.push(<Page className={classNames[array.length % 4]} title={lines[i]} content={content}/>)
     }
     array.push(<div />)
     setPages(array)
+    setCounter(Math.ceil(array.length/4))
   }
 
   const printHandler = () => {
@@ -39,6 +42,7 @@ function App() {
   return (
     <div className='container'>
       <textarea id='textArea' name='textArea' value={text} onChange={handleTextChange} placeholder='Start a line with "#" to create a new page'>    </textarea>
+      <p className='counter'>{counter}</p>
       <button className={"print"} onClick={printHandler}>PDF</button>
       <div className={"grid"} ref={pdfRef}>
         {pages}
